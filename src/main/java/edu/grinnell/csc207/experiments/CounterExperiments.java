80e276653400931ac78749653a3ac4f7b1714ec5
Cade <johnston2@grinnell.edu>
2024-10-01 15:46:07 -0500

    Add DblCtr.

--- src/main/java/edu/grinnell/csc207/experiments/CounterExperiments.java
+++ src/main/java/edu/grinnell/csc207/experiments/CounterExperiments.java
@@ -7,6 +7,7 @@ import edu.grinnell.csc207.util.BoundedCounter;
 import edu.grinnell.csc207.util.Tally;
 import edu.grinnell.csc207.util.NamedCounter;
 import edu.grinnell.csc207.util.DoubleCounter;
+import edu.grinnell.csc207.util.DblCtr;
 
 import java.io.PrintWriter;
 
@@ -26,7 +27,7 @@ public class CounterExperiments {
 
     // Set up some counters
     Counter alpha = new NamedCounter("alfa", 0);
-    Counter beta = new DoubleCounter(123);
+    Counter beta = new DblCtr(new DblCtr(new BasicCounter(0)));
     BasicCounter gamma = new BoundedCounter(-5, 3);
 
     // Print original values
